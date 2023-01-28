5ff65619bb2f4f89d3e97a88ba7fc254f10cbe45
Aydanshj <121218604+Aydanshj@users.noreply.github.com>
2023-01-29 00:46:00 +0400

    Add files via upload

---
+++ Practice/Task 26.01/Bicycle.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practice
+{
+    internal class Bicycle
+    {
+        internal class Bicycle
+        {
+            public string Brand;
+            public string Model;
+            public double Mileage;
+
+
+            public void Drive(double km)
+            {
+                this.Mileage += km;
+            }
+        }
+    }
+}
---
+++ Practice/Task 26.01/Notebook.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using System.Xml.Linq;
+
+namespace Practice
+{
+    internal class Notebook:Product
+    {
+        public Notebook(string name, double price) : base(name, price)
+        {
+
+        }
+        public int RAM;
+        public int Storage;
+
+        public override void ShowInfo()
+        {
+            Console.WriteLine($"Name: {Name} - Price: {Price} - RAM: {RAM} - Storage: {Storage}");
+        }
+    }
+}
